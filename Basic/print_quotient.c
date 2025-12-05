e5e53cfe4f5f0de1dc3b4a2f0ed19fe39f5e5dda
Henil Vaghela <vaghelahenil7@gmail.com>
2025-12-05 20:55:04 +0530

    program to print quotient of two integers

---
+++ Basic/print_quotient.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main() {
+    int a, b;
+    printf("Enter two integers: ");
+    scanf("%d %d", &a, &b);
+
+    if (b != 0)
+        printf("Quotient = %d", a / b);
+    else
+        printf("Error: division by zero!");
+
+    return 0;
+}
